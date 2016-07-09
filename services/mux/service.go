2b73bdb7b7f129d47eb6cdd614d1403e2fa71751
Scott Kidder <scott@mux.com>
2016-07-09 07:17:24 -0700

    Add unit tests for Mux service

--- services/mux/service.go
+++ services/mux/service.go
@@ -52,60 +52,21 @@ func (s *Service) Global() bool {
 }
 
 func (s *Service) Alert(incidentKey string, level kapacitor.AlertLevel, t time.Time) error {
-	if !incidentKeyRegExp.MatchString(incidentKey) {
-		return fmt.Errorf("Incident key did not match regular-expression pattern: key = %s", incidentKey)
-	}
-
-	// parse incident key for details
-	keyParts := incidentKeyRegExp.FindStringSubmatch(incidentKey)
-
-	parent := make(map[string]map[string]interface{})
-	pData := make(map[string]interface{})
-
-	// set breakdowns on incident
-	breakdownKey := strings.TrimSuffix(keyParts[3], ",")
-	breakdowns := strings.Split(breakdownKey, ",")
-	breakdownGroups := make([]map[string]string, len(breakdowns))
-	for _, b := range breakdowns {
-		breakdownParts := strings.Split(b, "=")
-		breakdownGroups = append(breakdownGroups, map[string]string{"name": breakdownParts[0], "value": breakdownParts[1]})
-	}
-
-	parent["incident"] = pData
-	pData["breakdown_key"] = breakdownKey
-	pData["breakdowns"] = breakdownGroups
-	switch level {
-	case kapacitor.WarnAlert:
-		pData["status"] = "open"
-		pData["severity"] = "warning"
-		pData["started_at"] = t.Format(time.RFC3339)
-	case kapacitor.CritAlert:
-		pData["status"] = "open"
-		pData["severity"] = "alert"
-		pData["started_at"] = t.Format(time.RFC3339)
-	case kapacitor.InfoAlert:
-		return fmt.Errorf("AlertLevel 'info' is currently ignored by the Mux service")
-	default:
-		pData["status"] = "closed"
-		pData["resolved_at"] = t.Format(time.RFC3339)
-	}
-
-	// Post data to Mux
-	var post bytes.Buffer
-	enc := json.NewEncoder(&post)
-	err := enc.Encode(parent)
+	incidentBytes, err := buildIncident(incidentKey, level, t)
 	if err != nil {
 		return err
 	}
 
-	// incidentKey should look like properties/:property_id/alerts/:alert_id
-	fullURL := s.url
-	if false == strings.HasSuffix(fullURL, "/") {
-		fullURL = fullURL + "/"
-	}
-	fullURL = fullURL + "internal-api/v1/properties/" + keyParts[1] + "/alerts/" + keyParts[2] + "/incident"
+	post := bytes.NewReader(incidentBytes)
 
-	req, err := http.NewRequest(http.MethodPost, fullURL, &post)
+	var fullURL string
+	fullURL, err = s.buildIncidentURL(incidentKey)
+	if err != nil {
+		return err
+	}
+
+	var req *http.Request
+	req, err = http.NewRequest(http.MethodPost, fullURL, post)
 	req.SetBasicAuth(s.username, s.password)
 	req.Header.Add("Content-Type", "application/json")
 	req.Header.Add("Accept", "application/json")
@@ -132,4 +93,61 @@ func (s *Service) Alert(incidentKey string, level kapacitor.AlertLevel, t time.T
 		return errors.New(r.Message)
 	}
 	return nil
+}
+
+func buildIncident(incidentKey string, level kapacitor.AlertLevel, t time.Time) ([]byte, error) {
+	if !incidentKeyRegExp.MatchString(incidentKey) {
+		return nil, fmt.Errorf("Incident key did not match regular-expression pattern: key = %s", incidentKey)
+	}
+	// parse incident key for details
+	keyParts := incidentKeyRegExp.FindStringSubmatch(incidentKey)
+
+	parent := make(map[string]map[string]interface{})
+	pData := make(map[string]interface{})
+
+	// set breakdowns on incident
+	breakdownKey := strings.TrimSuffix(keyParts[3], ",")
+	breakdowns := strings.Split(breakdownKey, ",")
+	breakdownGroups := make([]map[string]string, 0)
+	for _, b := range breakdowns {
+		breakdownParts := strings.Split(b, "=")
+		breakdownGroups = append(breakdownGroups, map[string]string{"name": breakdownParts[0], "value": breakdownParts[1]})
+	}
+
+	parent["incident"] = pData
+	pData["breakdown_key"] = breakdownKey
+	pData["breakdowns"] = breakdownGroups
+	switch level {
+	case kapacitor.WarnAlert:
+		pData["status"] = "open"
+		pData["severity"] = "warning"
+		pData["started_at"] = t.Format(time.RFC3339)
+	case kapacitor.CritAlert:
+		pData["status"] = "open"
+		pData["severity"] = "alert"
+		pData["started_at"] = t.Format(time.RFC3339)
+	case kapacitor.InfoAlert:
+		return nil, fmt.Errorf("AlertLevel 'info' is currently ignored by the Mux service")
+	default:
+		pData["status"] = "closed"
+		pData["resolved_at"] = t.Format(time.RFC3339)
+	}
+
+	return json.Marshal(parent)
+}
+
+func (s *Service) buildIncidentURL(incidentKey string) (string, error) {
+	if !incidentKeyRegExp.MatchString(incidentKey) {
+		return "", fmt.Errorf("Incident key did not match regular-expression pattern: key = %s", incidentKey)
+	}
+	// parse incident key for details
+	keyParts := incidentKeyRegExp.FindStringSubmatch(incidentKey)
+
+	// incidentKey should look like properties/:property_id/alerts/:alert_id
+	fullURL := s.url
+	if false == strings.HasSuffix(fullURL, "/") {
+		fullURL = fullURL + "/"
+	}
+	fullURL = fullURL + "internal-api/v1/properties/" + keyParts[1] + "/alerts/" + keyParts[2] + "/incident"
+	return fullURL, nil
 }
